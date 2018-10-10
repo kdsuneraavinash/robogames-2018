b300cf15c9482fb2d86618d0cb54eddbfa8e65c5
anju97chamantha <chamantha97anju@gmail.com>
2018-10-11 03:38:10 +0530

    Serial Connection Successful

--- WalkingRobot/app/src/main/java/com/stats/kdsuneraavinash/walkingrobot/CameraActivity.java
+++ WalkingRobot/app/src/main/java/com/stats/kdsuneraavinash/walkingrobot/CameraActivity.java
@@ -1,12 +1,15 @@
 package com.stats.kdsuneraavinash.walkingrobot;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -14,7 +17,10 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-enum IdentifyMode{
+import me.aflak.arduino.Arduino;
+import me.aflak.arduino.ArduinoListener;
+
+enum IdentifyMode {
     ROAD, JUNCTION
 }
 
@@ -23,7 +29,9 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
     private ArrowRecognition arrowRecognizer;
     private RoadRecognition roadRecognizer;
 
+    Arduino arduino;
     private IdentifyMode mode = IdentifyMode.ROAD;
+    private TextView textStatus;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -43,6 +51,51 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         }
     };
 
+    private View.OnClickListener buttonRoadModeOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mode = IdentifyMode.ROAD;
+        }
+    };
+
+    private View.OnClickListener buttonJunctionModeOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mode = IdentifyMode.JUNCTION;
+        }
+    };
+
+    private ArduinoListener arduinoListener = new ArduinoListener() {
+        @Override
+        public void onArduinoAttached(UsbDevice device) {
+            arduino.open(device);
+            textStatus.setBackgroundColor(Color.BLUE);
+        }
+
+        @Override
+        public void onArduinoDetached() {
+            textStatus.setBackgroundColor(Color.RED);
+
+        }
+
+        @Override
+        public void onArduinoMessage(final byte[] bytes) {
+            textStatus.setBackgroundColor(Color.GREEN);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textStatus.append(new String(bytes));
+                }
+            });
+
+        }
+
+        @Override
+        public void onArduinoOpened() {
+            textStatus.setBackgroundColor(Color.YELLOW);
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,18 +109,45 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
 
+        textStatus = findViewById(R.id.textIndicator);
+
         Button buttonRoadMode = findViewById(R.id.buttonRoadMode);
         Button buttonJunctionMode = findViewById(R.id.buttonJunctionMode);
-        buttonJunctionMode.setOnClickListener(new View.OnClickListener() {
+        buttonJunctionMode.setOnClickListener(buttonJunctionModeOnClick);
+        buttonRoadMode.setOnClickListener(buttonRoadModeOnClick);
+
+        findViewById(R.id.buttonForward).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                mode = IdentifyMode.JUNCTION;
+                arduino.send("1".getBytes());
             }
         });
-        buttonRoadMode.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.buttonStop).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                mode = IdentifyMode.ROAD;
+                arduino.send("5".getBytes());
             }
         });
+        findViewById(R.id.buttonLeft).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arduino.send("3".getBytes());
+            }
+        });
+        findViewById(R.id.buttonRight).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arduino.send("4".getBytes());
+            }
+        });
+
+        arduino = new Arduino(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        arduino.setArduinoListener(arduinoListener);
     }
 
     @Override
@@ -88,6 +168,8 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         super.onDestroy();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
+        arduino.unsetArduinoListener();
+        arduino.close();
     }
 
     @Override
@@ -101,16 +183,14 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat screen;
-        if (mode == IdentifyMode.ROAD){
+        if (mode == IdentifyMode.ROAD) {
             screen = roadRecognizer.process(inputFrame.rgba());
-        }else if (mode == IdentifyMode.JUNCTION){
+        } else if (mode == IdentifyMode.JUNCTION) {
             screen = arrowRecognizer.process(inputFrame.rgba());
-        }else{
+        } else {
             screen = inputFrame.rgba();
         }
         return screen;
-
-//        return arrowRecognizer.process(inputFrame.rgba());
     }
 }
 
